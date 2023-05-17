821ef8bf4eab6f3cb09b9315a16ff25b213192cf
AB-DEV28 <bouguerraabdessamed2@gmail.com>
2023-05-17 22:17:48 +0100

    mlm

--- includes/NavbarOfContent.php
+++ includes/NavbarOfContent.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="../index.php">Quizy</a>
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="../index.php"><img src="../images/navbarLogo.svg" alt="..." /></a>
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
                 <a class="nav-link" href="../includes/logOut.php">Sign out</a>
