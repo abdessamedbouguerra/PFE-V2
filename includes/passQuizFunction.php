de6079458e0dc84ebe8e15c375c0475f72b873e4
AB-DEV28 <bouguerraabdessamed2@gmail.com>
2023-05-25 20:06:47 +0100

    pass quiz functions is done

--- includes/passQuizFunction.php
+++ includes/passQuizFunction.php
@@ -13,17 +13,27 @@ $list_id_answers = array();
 // Initialize variables for note (score) and date of quiz passage
 $note=0;
 $data_passage=date("Y-m-d");
+// Insert the user's quiz result (including ID, quiz ID, score and date) into the database
+$requette = "INSERT INTO `pass_quiz`(`id_user`,`id_quiz`,`date`,`note`) 
+ VALUES('$id_User','$id_Q','$data_passage','$note')";
 
+// Check if the SQL query executed successfully or not
+$resultat = $conn->query($requette);
+if ($resultat) {
+    $er=0;
+} else {
+    $er=1;
+}
+$id_pass_tmp=$conn->lastInsertId();
 // Loop through each question in the quiz
 for ($i = 0; $i <$number_question; $i++) {
+    if (isset($_POST['answer_Question_' . $i . ''])) {
     // Get the ID of the current question and answer selected by the user from the form
     $id_questions=$_POST['Question_' . $i . ''];
     $answers=$_POST['answer_Question_' . $i . ''];
-    
     // Retrieve the correct answer for the current question from the database
     $requette = "SELECT answer FROM question WHERE id_question='$id_questions'";
     $resultat = $conn->query($requette);
-    
     // Check if the SQL query executed successfully or not
     if ($resultat) {
         $er=0;
@@ -46,8 +56,8 @@ for ($i = 0; $i <$number_question; $i++) {
     }
     
     // Insert the user's answer for the current question into the database
-    $requette2 = "INSERT INTO `answers_users`(`id_user`,`id_quiz`, `id_question`, `answer`, `answer_case`) 
-         VALUES('" . $id_User . "','" . $id_Q . "','" . $id_questions. "','" . $answers. "','" . $answer_case . "')";
+    $requette2 = "INSERT INTO `answers_users`(`id_user`,`id_quiz`, `id_question`, `answer`, `answer_case` , `id_pass_quiz`) 
+         VALUES('$id_User','$id_Q','$id_questions','$answers','$answer_case','$id_pass_tmp')";
          
          // Check if the SQL query executed successfully or not
          $resultat2 = $conn->query($requette2);
@@ -56,25 +66,10 @@ for ($i = 0; $i <$number_question; $i++) {
         } else {
             $er=1;
         }
-        $list_id_answers[$i]=$conn->lastInsertId();
+    } 
 }
-
-// Insert the user's quiz result (including ID, quiz ID, score and date) into the database
-$requette = "INSERT INTO `pass_quiz`(`id_user`,`id_quiz`,`date`,`note`) 
- VALUES('" . $id_User . "','" . $id_Q . "','" . $data_passage . "','" . $note . "')";
-
-// Check if the SQL query executed successfully or not
-$resultat = $conn->query($requette);
-if ($resultat) {
-    $er=0;
-} else {
-    $er=1;
-}
-$id_pass_tmp=$conn->lastInsertId();
-for ($i = 0; $i <$number_question; $i++) {
-    $requette3 ="UPDATE `answers_users` SET id_pass_quiz='$id_pass_tmp' WHERE id_answer_user=$list_id_answers[$i]";
-    $resultat3 =$conn->query($requette3);
-}
+    $requette3 ="UPDATE `pass_quiz` SET note='$note' WHERE id_pass=$id_pass_tmp";
+    $resultat3=$conn->query($requette3);
 // Check if there were any errors during the process and display an error message accordingly
 if ($er) {
     echo "pessage failed: " . $e->getMessage();
